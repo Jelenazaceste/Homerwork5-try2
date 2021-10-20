000878c64ecacb8ad6284b7bab0aebe55852c594
Jelenazaceste <jel.zaceste@gmail.com>
2021-10-20 12:03:49 +0300

    Add project files.

---
+++ Homerwork5 try2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homerwork5_try2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a side of a square: ");
+            int Side = Convert.ToInt32(Console.ReadLine());
+            int Area = Side * Side;
+            Console.WriteLine($"Area of square side {Side} is {Area}");
+            Console.ReadKey();
+        }
+    }
+}
